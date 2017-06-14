07fb37545ccc92473165c95b0256e0255d659f5e
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-06-14 09:36:25 +0200

    added table name

--- src/main/java/com/greenfox/notification/model/Heartbeat.java
+++ src/main/java/com/greenfox/notification/model/Heartbeat.java
@@ -5,10 +5,12 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "heartbeat")
 public class Heartbeat {
   @Id
   private Boolean status;
