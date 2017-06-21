71d9b2483c802f4037f276fca65616ae58a5157b
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-06-21 11:12:56 +0200

    refactor(remove unused lines)

--- src/main/java/com/greenfox/notification/controller/HeartbeatController.java
+++ src/main/java/com/greenfox/notification/controller/HeartbeatController.java
@@ -55,20 +55,13 @@ public class HeartbeatController {
     mail.setTemplateId("0783a75f-044a-4725-8624-425cc41b7735");
     SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
     Request request = new Request();
-    try {
-      request.setMethod(Method.POST);
-      request.setEndpoint("mail/send");
-      request.setBody(mail.build());
-      Response response = sg.api(request);
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getBody());
-      System.out.println(response.getHeaders());
-    } catch (IOException ex) {
-      throw ex;
-    }
-
+    request.setMethod(Method.POST);
+    request.setEndpoint("mail/send");
+    request.setBody(mail.build());
+    Response response = sg.api(request);
+    System.out.println(response.getStatusCode());
+    System.out.println(response.getBody());
+    System.out.println(response.getHeaders());
     return data;
-
   }
-
 }
