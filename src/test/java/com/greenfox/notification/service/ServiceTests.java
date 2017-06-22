e508443c5e560cb98271b08124dc5148110c9f64
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-06-22 16:12:45 +0200

    Made a basic test for generateEmail, it checks if the handmade generated mail is the same as the method generated one

--- src/test/java/com/greenfox/notification/service/ServiceTests.java
+++ src/test/java/com/greenfox/notification/service/ServiceTests.java
@@ -1,10 +1,17 @@
 package com.greenfox.notification.service;
 
+import com.greenfox.notification.model.classes.Attribute;
+import com.greenfox.notification.model.classes.Data;
 import com.greenfox.notification.model.classes.DatabaseResponse;
 import com.greenfox.notification.repository.HeartbeatRepository;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
+import com.sendgrid.Personalization;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -26,9 +33,13 @@ public class ServiceTests {
   private final String HOSTNAME = System.getenv("HOSTNAME");
   private String queueName = System.getenv("QUEUE_NAME");
   private HttpServletRequest requestMock;
+  private EmailGenerator emailGeneratorMock;
+  private Mail testMailMock;
 
   @Before
   public void setup() throws Exception {
+    testMailMock = Mockito.mock(Mail.class);
+    emailGeneratorMock = Mockito.mock(EmailGenerator.class);
     heartbeatRepositoryMock = Mockito.mock(HeartbeatRepository.class);
     timeStampServiceMock = Mockito.mock(TimeStampService.class);
     rabbitMQMock = Mockito.mock(RabbitMQ.class);
@@ -86,8 +97,8 @@ public class ServiceTests {
   public void testLogWithPrintOut() throws Exception {
     Log log = new Log();
     when(requestMock.getRequestURI()).thenReturn("/test");
-    log.info(requestMock,"test message");
-    assertEquals( "INFO " + log.getDateTime() + " " + HOSTNAME + " " +
+    log.info(requestMock, "test message");
+    assertEquals("INFO " + log.getDateTime() + " " + HOSTNAME + " " +
             "test message " + "HTTP-REQUEST " + "/test", outContent.toString().trim());
   }
 
@@ -95,8 +106,31 @@ public class ServiceTests {
   public void testLogWithErrorPrintOut() throws Exception {
     Log log = new Log();
     when(requestMock.getRequestURI()).thenReturn("/test");
-    log.error(requestMock,"test message with error");
+    log.error(requestMock, "test message with error");
     assertEquals("ERROR " + log.getDateTime() + " " + HOSTNAME + " " +
             "test message with error " + "HTTP-ERROR " + "/test", errContent.toString().trim());
+  }
+
+  @Test
+  public void testGenerateEmail() throws Exception {
+    Data testData = new Data("testType", new Attribute("testEmail@test.com", "testName", "testUrl.com"));
+    EmailGenerator testGenerator = new EmailGenerator();
+    Mail mail = new Mail();
+    Email fromEmail = new Email();
+    fromEmail.setEmail(System.getenv("EMAIL_ADDRESS"));
+    mail.setFrom(fromEmail);
+    Personalization personalization = new Personalization();
+    Email to = new Email();
+    to.setEmail(testData.getAttributes().getEmail());
+    personalization.addTo(to);
+    mail.addPersonalization(personalization);
+    mail.setSubject("Registration process");
+    mail.personalization.get(0).addSubstitution("-name-", testData.getAttributes().getName());
+    mail.setTemplateId(System.getenv("TEMPLATE_ID"));
+    Content content = new Content();
+    content.setType("text/plain");
+    content.setValue("vilmoskorte");
+    mail.addContent(content);
+    Assert.assertEquals(mail.build(), testGenerator.generateEmail(testData).build());
   }
 }
