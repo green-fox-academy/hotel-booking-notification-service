55e9eed8258db519335fc9c28e845a12cd781e00
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-06-23 13:05:15 +0200

    Made a test for Sending Email

--- src/test/java/com/greenfox/notification/service/ServiceTests.java
+++ src/test/java/com/greenfox/notification/service/ServiceTests.java
@@ -6,23 +6,25 @@ import com.greenfox.notification.model.classes.DatabaseResponse;
 import com.greenfox.notification.repository.HeartbeatRepository;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.sendgrid.Content;
-import com.sendgrid.Email;
-import com.sendgrid.Mail;
-import com.sendgrid.Personalization;
+import com.sendgrid.*;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 public class ServiceTests {
   private HeartbeatRepository heartbeatRepositoryMock;
@@ -33,17 +35,27 @@ public class ServiceTests {
   private final String HOSTNAME = System.getenv("HOSTNAME");
   private String queueName = System.getenv("QUEUE_NAME");
   private HttpServletRequest requestMock;
-  private EmailGenerator emailGeneratorMock;
-  private Mail testMailMock;
+  private SendGrid mockSg;
+  private Request mockRequest;
+  private Response mockResponse;
+  private EmailSenderService emailSenderServiceMock;
+  private Mail mockMail;
+  private EmailGenerator mockEmailGenerator;
+  private Data mockData;
 
   @Before
   public void setup() throws Exception {
-    testMailMock = Mockito.mock(Mail.class);
-    emailGeneratorMock = Mockito.mock(EmailGenerator.class);
     heartbeatRepositoryMock = Mockito.mock(HeartbeatRepository.class);
     timeStampServiceMock = Mockito.mock(TimeStampService.class);
     rabbitMQMock = Mockito.mock(RabbitMQ.class);
     requestMock = Mockito.mock(HttpServletRequest.class);
+    mockSg = Mockito.mock(SendGrid.class);
+    mockRequest = Mockito.mock(Request.class);
+    mockResponse = Mockito.mock(Response.class);
+    emailSenderServiceMock = Mockito.mock(EmailSenderService.class);
+    mockMail = Mockito.mock(Mail.class);
+    mockEmailGenerator = Mockito.mock(EmailGenerator.class);
+    mockData = Mockito.mock(Data.class);
     ConnectionFactory factory = new ConnectionFactory();
     factory.setUri(System.getenv("RABBITMQ_BIGWIG_TX_URL"));
     Connection connection = factory.newConnection();
@@ -133,4 +145,20 @@ public class ServiceTests {
     mail.addContent(content);
     Assert.assertEquals(mail.build(), testGenerator.generateEmail(testData).build());
   }
+
+  @Test
+  public void testSendEmail() throws Exception {
+    MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest("/post", "/testendpoint");
+    when(mockEmailGenerator.generateEmail(mockData)).thenReturn(mockMail);
+    doAnswer(invocation -> {
+      Object[] args = invocation.getArguments();
+      System.out.println("called with arguments: " + Arrays.toString(args));
+      return null;
+    }).when(mockRequest).setEndpoint("mail/send");
+    when(mockSg.api(mockRequest)).thenReturn(mockResponse);
+    emailSenderServiceMock.sendConfirmationEmail(mockHttpServletRequest, mockData);
+    verify(mockSg.api(mockRequest));
+    assertThat(mockMail, is(notNullValue()));
+  }
 }
+
