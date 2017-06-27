1b57915078d525ca9d90d5b68a8b9ad5a3bd9064
tamasbrandstadter <tamasbrandstadter@gmail.com>
2017-06-27 09:53:35 +0200

    finished endpoint aspect logging

--- src/main/java/com/greenfox/notification/service/MyAspect.java
+++ src/main/java/com/greenfox/notification/service/MyAspect.java
@@ -21,14 +21,28 @@ public class MyAspect {
   @AfterReturning(
           pointcut = "execution(* com.greenfox.notification.controller.HeartbeatController.getHeartbeats(..))",
           returning = "retVal")
-  public void myMethod(JoinPoint joinPoint, Response retVal) {
-    log.info("/heartbeat" +  joinPoint.getSignature().getName(), retVal.toString());
+  public void logInfoHeartbeat(JoinPoint joinPoint, Response retVal) {
+    log.info("/heartbeat " + joinPoint.getSignature().getName() + " method", retVal.toString());
   }
 
   @AfterThrowing(
           pointcut = "execution(* com.greenfox.notification.controller.HeartbeatController.getHeartbeats(..))",
-          throwing= "error")
-  public void doRecoveryActions(JoinPoint joinPoint, Exception error) {
-    log.error("/heartbeat" +  joinPoint.getSignature().getName(), error.getMessage());
+          throwing = "error")
+  public void logErrorHeartbeat(JoinPoint joinPoint, Exception error) {
+    log.error("/heartbeat " + joinPoint.getSignature().getName() + " method", error.getMessage());
+  }
+
+  @AfterReturning(
+          pointcut = "execution(* com.greenfox.notification.controller.RegistrationConfirmationController.registration(..))",
+          returning = "retVal")
+  public void logInfoRegistration(JoinPoint joinPoint, Response retVal) {
+    log.info("/email/registration " + joinPoint.getSignature().getName() + " method", retVal.toString());
+  }
+
+  @AfterThrowing(
+          pointcut = "execution(* com.greenfox.notification.controller.HeartbeatController.getHeartbeats(..))",
+          throwing = "error")
+  public void logErrorRegistration(JoinPoint joinPoint, Exception error) {
+    log.error("/email/registration " + joinPoint.getSignature().getName() + " method", error.getMessage());
   }
 }
