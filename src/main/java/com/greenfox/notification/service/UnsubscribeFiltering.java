654573655a6112de04d991d2fab5aaf6cc707b07
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-07-05 10:11:17 +0200

    Made a new querry to repo and created a new service for filtering

---
+++ src/main/java/com/greenfox/notification/service/UnsubscribeFiltering.java
@@ -0,0 +1,20 @@
+package com.greenfox.notification.service;
+
+import com.greenfox.notification.model.classes.heartbeat.Data;
+import com.greenfox.notification.repository.UnsubscribeDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UnsubscribeFiltering {
+  private final UnsubscribeDataRepository unsubscribeDataRepository;
+
+  @Autowired
+  public UnsubscribeFiltering(UnsubscribeDataRepository unsubscribeDataRepository) {
+    this.unsubscribeDataRepository = unsubscribeDataRepository;
+  }
+
+  public boolean checkIfUserUnsubscribed(Data data) {
+    return unsubscribeDataRepository.findByAttributes_Email(data.getAttributes().getEmail());
+  }
+}
